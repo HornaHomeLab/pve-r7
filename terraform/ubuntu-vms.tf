8f77e6957db113700849c06a8c64f5f4eb7a86a5
Stanisław Horna <stanislawhorna@outlook.com>
2025-07-24 22:54:21 +0200

    add k3s cluster

--- terraform/ubuntu-vms.tf
+++ terraform/ubuntu-vms.tf
@@ -1,12 +1,28 @@
 locals {
   ubuntu_vms = {
-    "docker-vm" = {
-      vm_desc      = "test VM for docker"
+    "k3s-server-test" = {
+      vm_desc      = "test k3s server"
       ip_address   = "10.0.10.220"
       cidr_netmask = "24"
       gateway      = "10.0.10.1"
       dns_servers  = ["10.0.10.11", "1.1.1.1"]
-      tags         = ["ubuntu", "docker"]
+      tags         = ["ubuntu", "k3s_cluster_server"]
+    }
+    "k3s-agent1-test" = {
+      vm_desc      = "test k3s agent"
+      ip_address   = "10.0.10.221"
+      cidr_netmask = "24"
+      gateway      = "10.0.10.1"
+      dns_servers  = ["10.0.10.11", "1.1.1.1"]
+      tags         = ["ubuntu", "k3s_cluster_agent"]
+    }
+    "k3s-agent2-test" = {
+      vm_desc      = "test k3s agent"
+      ip_address   = "10.0.10.222"
+      cidr_netmask = "24"
+      gateway      = "10.0.10.1"
+      dns_servers  = ["10.0.10.11", "1.1.1.1"]
+      tags         = ["ubuntu", "k3s_cluster_agent"]
     }
   }
 }
