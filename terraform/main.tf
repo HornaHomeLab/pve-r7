d377f10a6af39d441dc3274a10039b89c7090a6e
Stanisław Horna <stanislawhorna@outlook.com>
2025-07-24 18:58:19 +0200

    add sample ubuntu VM to deploy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "ubuntu_vm" {
-  source            = "github.com/HornaHomeLab/Terraform_Modules/ubuntu-vm"
+  source            = "github.com/StanislawHorna/Terraform/ubuntu-vm"
   target_node       = "pve-r7"
   hcp_vault_role_id = var.vault_role_id
   hcp_vault_secret  = var.vault_secret
@@ -9,7 +9,7 @@ module "ubuntu_vm" {
   vm_name      = each.key
   vm_desc      = lookup(each.value, "vm_desc", null)
   vmid         = lookup(each.value, "vmid", null)
-  tags         = lookup(each.value, "tags", null)
+  tags         = lookup(each.value, "tags", ["ubuntu"])
   cores        = lookup(each.value, "cores", 4)
   memory       = lookup(each.value, "memory", 4096)
   disk_size    = lookup(each.value, "disk_size", "64G")
@@ -17,4 +17,18 @@ module "ubuntu_vm" {
   cidr_netmask = lookup(each.value, "cidr_netmask", null)
   gateway      = lookup(each.value, "gateway", null)
   dns_servers  = lookup(each.value, "dns_servers", ["1.1.1.1"])
+}
+
+module "ansible_inventory" {
+  source       = "github.com/StanislawHorna/Terraform/ansible-vm-inventory"
+  ubuntu_vms   = local.ubuntu_vms
+  ansible_user = "proxmox"
+  output_path  = "${path.module}/../ansible/inventory/default.ini"
+}
+
+module "ansible_k3s_inventory" {
+  source       = "github.com/StanislawHorna/Terraform/ansible-k3s-inventory"
+  ubuntu_vms   = local.ubuntu_vms
+  ansible_user = "proxmox"
+  output_path  = "${path.module}/../ansible/inventory"
 }
--- terraform/ubuntu-vms.tf
+++ terraform/ubuntu-vms.tf
@@ -1,10 +1,12 @@
 locals {
   ubuntu_vms = {
-    # "test-new-provisioning" = {
-    #   vm_desc      = "test"
-    #   ip_address   = "10.0.10.123"
-    #   cidr_netmask = "24"
-    #   gateway      = "10.0.10.1"
-    # }
+    "docker-vm" = {
+      vm_desc      = "test VM for docker"
+      ip_address   = "10.0.10.220"
+      cidr_netmask = "24"
+      gateway      = "10.0.10.1"
+      dns_servers  = ["10.0.10.11", "1.1.1.1"]
+      tags         = ["ubuntu", "docker"]
+    }
   }
 }
