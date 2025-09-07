81a964a9802e211082eb04d4a969208d85c3c6b2
Stanisław Horna <stanislawhorna@outlook.com>
2025-09-07 20:17:24 +0200

    add hcp cluster vms

--- terraform/ubuntu-vms.tf
+++ terraform/ubuntu-vms.tf
@@ -1,13 +1,31 @@
 locals {
   ubuntu_vms = {
-    "nomad-01" = {
+    "hcp-01" = {
       target_node  = "pve-r7"
       vm_desc      = "HCP Nomad node"
-      ip_address   = "10.0.10.220"
+      ip_address   = "10.0.10.221"
       cidr_netmask = "24"
       gateway      = "10.0.10.1"
       dns_servers  = ["10.0.10.11", "1.1.1.1"]
-      tags         = ["ubuntu", "nomad", "docker"]
+      tags         = ["ubuntu", "nomad"]
+    },
+    "hcp-02" = {
+      target_node  = "pve-r7"
+      vm_desc      = "HCP Nomad node"
+      ip_address   = "10.0.10.222"
+      cidr_netmask = "24"
+      gateway      = "10.0.10.1"
+      dns_servers  = ["10.0.10.11", "1.1.1.1"]
+      tags         = ["ubuntu", "nomad"]
+    },
+    "hcp-03" = {
+      target_node  = "pve-r7"
+      vm_desc      = "HCP Nomad node"
+      ip_address   = "10.0.10.223"
+      cidr_netmask = "24"
+      gateway      = "10.0.10.1"
+      dns_servers  = ["10.0.10.11", "1.1.1.1"]
+      tags         = ["ubuntu", "nomad"]
     }
   }
 }
