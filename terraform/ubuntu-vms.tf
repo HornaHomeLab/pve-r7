709f4ab4765c791b74843c5658a6f8d5aa62a312
Stanisław Horna <stanislawhorna@outlook.com>
2025-09-09 22:49:20 +0200

    replace nomad cluster with k3s

--- terraform/ubuntu-vms.tf
+++ terraform/ubuntu-vms.tf
@@ -1,31 +1,40 @@
 locals {
   ubuntu_vms = {
-    "hcp-01" = {
+    "k3s-01" = {
       target_node  = "pve-r7"
-      vm_desc      = "HCP Nomad node"
+      vm_desc      = "K3s production server"
+      vmid         = 221
+      memory       = 4096
+      cores        = 2
       ip_address   = "10.0.10.221"
       cidr_netmask = "24"
       gateway      = "10.0.10.1"
       dns_servers  = ["10.0.10.11", "1.1.1.1"]
-      tags         = ["ubuntu", "nomad_server"]
+      tags         = ["ubuntu", "k3s_cluster_prod_server"]
     },
-    "hcp-02" = {
+    "k3s-02" = {
       target_node  = "pve-r7"
-      vm_desc      = "HCP Nomad node"
+      vm_desc      = "K3s production agent"
+      vmid =         222
+      memory       = 8192
+      cores        = 4
       ip_address   = "10.0.10.222"
       cidr_netmask = "24"
       gateway      = "10.0.10.1"
       dns_servers  = ["10.0.10.11", "1.1.1.1"]
-      tags         = ["ubuntu", "nomad_client"]
+      tags         = ["ubuntu", "k3s_cluster_prod_agent"]
     },
-    "hcp-03" = {
+    "k3s-03" = {
       target_node  = "pve-r7"
-      vm_desc      = "HCP Nomad node"
+      vm_desc      = "K3s production agent"
+      vmid =         223
+      memory       = 8192
+      cores        = 4
       ip_address   = "10.0.10.223"
       cidr_netmask = "24"
       gateway      = "10.0.10.1"
       dns_servers  = ["10.0.10.11", "1.1.1.1"]
-      tags         = ["ubuntu", "nomad_client"]
+      tags         = ["ubuntu", "k3s_cluster_prod_agent"]
     }
   }
 }
